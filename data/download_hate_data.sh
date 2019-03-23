92c6d9e0bce7eda74be7dc7fe76de7219df600ce
benoriol <benet.oriol.sabat@gmail.com>
2019-03-23 18:00:51 +0100

    download hate data

---
+++ data/download_hate_data.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+
+echo "For installation instructions visit https://github.com/hardikvasa/google-images-download"
+
+googleimagesdownload -k "racist meme" -l 1000
+googleimagesdownload -k "muslim meme" -l 1000
